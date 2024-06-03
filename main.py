4f8a934b8f902ae102cbaf9d6b8981761ea23b2c
Kavin <Kavi448SU15@coral.com>
2024-06-03 09:51:25 +0530

    thread added

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
+from threading import Thread
+import time
 
 app = FastAPI()
 
-
+count = 0
 
 @app.get("/")
 async def root():
-    return {"detail": "API running..."}
+    return {"detail": f"Current count is {count}"}
+
+def first_fun():
+    while 1:
+        time.sleep(3)
+        global count
+        count += 1
+        print(count)
+
+t1 = Thread(target=first_fun)
+t1.start()
+t1.join()
